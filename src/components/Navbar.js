a5a5bacd516c03a5951b82bf03847703971c2109
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-01-27 12:07:21 -0600

    Add navbar component and route to app.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
-                        
+                        TRVL <i className='fab fa-typo3' />
                     </Link>
                 </div>
             </nav>
