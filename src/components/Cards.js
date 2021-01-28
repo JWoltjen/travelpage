0f91f5887d0b470a4ceb1d761e59045b81facb7a
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-01-28 10:42:33 -0600

    add cards to landing page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -15,7 +15,33 @@ function Cards() {
                         label="Adventure"
                         path="/services"
                         />
+                        <CardItem
+                        src='images/img-2.jpg'
+                        text="Relax on a tropical island"
+                        label="Luxury"
+                        path="/services"
+                        />
                     </ul>
+                    <ul className='cards__items'>
+                        <CardItem
+                        src='images/img-1.jpg'
+                        text="Take a sunrise mountain hike"
+                        label="Adventure"
+                        path="/services"
+                        />
+                        <CardItem
+                        src='images/img-3.jpg'
+                        text="Set sail across the Atlantic"
+                        label="Adventure"
+                        path="/services"
+                        />
+                         <CardItem
+                        src='images/img-4.jpg'
+                        text="Participate in island soccer"
+                        label="Sport"
+                        path="/services"
+                        />
+                    </ul> 
                 </div>
             </div>
         </div>
